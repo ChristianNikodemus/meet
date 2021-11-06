6534e03ea6bafacd2ab4c42433b66aecb591ea9a
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-06 14:30:33 -0400

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,8 +19,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://YOUR_GITHUB_USERNAME.github.io/meet/"],
-  javascript_origins: ["https://YOUR_GITHUB_USERNAME.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://christiannikodemus.github.io/meet/"],
+  javascript_origins: ["https://christiannikodemus.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
