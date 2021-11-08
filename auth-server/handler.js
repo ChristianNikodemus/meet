fb44d3b2bf199bf2ae11f748d975b24ec6d27c1b
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-08 12:00:46 -0500

    Added CORS header to catch function of getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -146,6 +146,9 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         body: JSON.stringify(err),
       };
     });
