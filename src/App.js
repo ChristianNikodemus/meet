97fad779e2e000908eb7ed8a0ce1f3e843f0a564
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-08 13:12:18 -0500

    Added content to App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Welcome to Meet!</h1>
+      <p>A React Web Application Designed to Bring People Together</p>
     </div>
   );
 }
