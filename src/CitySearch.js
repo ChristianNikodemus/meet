8c234af475e9b22d1269970175a0d261ae020c4b
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-09 23:26:47 -0500

    Created test for search to render text

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: '',
+  }
+
+
   render() {
     return (
       <div className="CitySearch">
         <input
         type="text"
         className="city"
+        value={this.state.query}
         />
         <ul className="suggestions">
         </ul>
