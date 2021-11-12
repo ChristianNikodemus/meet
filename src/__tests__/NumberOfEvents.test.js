21def54313e31f502f4a68e8af28f194b25d3f55
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-11 21:02:05 -0500

    Unit test for 'render text input'

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NumberOfEvents from "../NumberOfEvents";
+
+describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
+
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test("render text input", () => {
+    expect(NumberOfEventsWrapper.find(".num-events")).toHaveLength(1);
+  });
+  
+});
