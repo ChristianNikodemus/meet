1664bf0e7ac8fb465a28a4dbfcbe0cc290ecf83a
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-23 23:07:53 -0500

    Added third senario

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,7 +6,11 @@ describe("show/hide an event details", () => {
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
+    });
     page = await browser.newPage();
     await page.goto("http://localhost:3002/");
     await page.waitForSelector(".event");
@@ -25,7 +29,6 @@ describe("show/hide an event details", () => {
   // Senario 2
   test("User can expand an event to see its details", async () => {
     await page.click(".event .details-btn");
-
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeDefined();
   });
