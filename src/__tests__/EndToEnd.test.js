313b0927329bd9b44d25fc4f851e4805570438c7
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-25 14:22:07 -0500

    Updated how I defined browser in my beforeAll

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -7,7 +7,7 @@ describe("show/hide an event details", () => {
   let browser;
   let page;
   beforeAll(async () => {
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
       headless: false,
       slowMo: 250, // slow down by 250ms
       ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
