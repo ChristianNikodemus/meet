8c2f00e823c0dae6c81a304cfc441030262645ea
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-25 21:12:06 -0500

    Removed error to throw to Atatus

--- src/index.js
+++ src/index.js
@@ -27,4 +27,5 @@ serviceWorkerRegistration.unregister();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-atatus.notify(new Error("Test Atatus Setup"));
+// This throws a test to Atatus to see if it's integrated correctly
+//atatus.notify(new Error("Test Atatus Setup"));
