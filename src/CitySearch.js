e075254db06f2791942550458e1c6623e5593789
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-27 18:16:11 -0500

    Implemented the Info Alert into Citysearch

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -32,13 +32,23 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
