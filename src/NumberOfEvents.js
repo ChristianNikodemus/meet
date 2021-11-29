0f36ab34eba8e9d55c5b27f726f32a250d198073
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-11-28 20:12:55 -0500

    Replaced slider with form input

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
-import RangeSlider from "react-bootstrap-range-slider";
+import { Form } from "react-bootstrap";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 12,
+    errorText: "",
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 12) {
+      this.setState({
+        numberOfEvents: "",
+        errorText: "Please enter a number between 1 and 12",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: "",
+      });
+    }
+    this.props.updateNumberOfEvents(event.target.value);
+  };
+
   render() {
     return (
       <div className="numberOfEvents">
-        <h3 className="events-text">Select the number of events</h3>
-        <RangeSlider
-          min={1}
-          max={12}
+        <p className="events-text">Number of events:</p>
+        <Form.Control
+          type="number"
+          name="number"
           className="num-events"
           value={this.props.numberOfEvents}
           onChange={(e) => this.props.updateEventCount(e)}
+          placeholder="12"
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
