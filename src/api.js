42bfe919c986c845d5272be49bcedc248660a439
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-12-10 00:20:22 -0500

    Changed the api to fetch data rather than events

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ export const getEvents = async (events) => {
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(events).events : [];
+    return data ? JSON.parse(data).events : [];
   }
 
   const token = await getAccessToken();
