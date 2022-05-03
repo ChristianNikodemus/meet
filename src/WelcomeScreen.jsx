2afe327452e8a4fcb10e37b6599f0fb14a1391a2
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-05-02 21:32:01 -0400

    Adjusted styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,7 +4,7 @@ import "./WelcomeScreen.css";
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1>
+      <h1 className="WelcomeScreen-text">Welcome to the Meet app</h1>
       <h4>
         Log in to see upcoming events around the world for full-stack developers
       </h4>
